192003d61131f233b60a098e85d4bcdbf94f1e0e
Luis Rodriguez <luisu_10@hotmail.com>
2022-09-19 23:24:51 -0500

    stores-map-feature-first-commit

---
+++ client/components/Store.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Store extends React.Component{
+
+    constructor(){
+        super();
+    }
+
+    render(){
+
+        return (
+            <h1>See stores</h1>
+        )
+    }
+
+}
+
+export default Store;
