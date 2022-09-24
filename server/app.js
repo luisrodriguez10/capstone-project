4a16159050bf11934dc8ae65e912d459df9a6a7c
Alexander Stoisolovich <alexander.stoisolovich@gmail.com>
2022-09-24 11:08:32 -0400

    Commented out .env and axios call for deployment

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
-const axios = require('axios');
+// const axios = require('axios');
 module.exports = app
 
 // logging middleware
